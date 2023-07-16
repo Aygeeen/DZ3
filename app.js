c97d9757735ca3d8bde072f0d3d67a291661c12b
Aygeeen <wreckingkraken@gmail.com>
2023-07-16 21:17:09 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function getDataType(str) {
+    return console.log(typeof str)
+}
+getDataType(2)
+//-------------------------------------//
+function reverse(string) {
+    return string.split('').reverse().join('');
+}
+console.log(reverse(prompt('Введите строку, чтобы ее перевернуть:')))
+
